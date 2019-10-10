adf7b2cac9974cb856784f386850f67b52b6ebb3
glukomania <glukomani@gmail.com>
2019-10-10 14:21:07 +0200

    map and hotels are ready

---
+++ js/map.js
@@ -0,0 +1,66 @@
+const hotels = [
+	{name: `Hotel Labe`, type: `HOTEL`, price: `600`, stars: `2`, rate: `8.3`, pictures: [], coords: `50°52'27.542\"N,14°14'12.724\"E`, web: ``},
+	{name: `Hotel Hawel Hřensko`, type: `HOTEL`, price: `850`, stars: `2`, rate: ``, pictures: [], coords: `50°52'26.402\"N,14°14'17.634\"E`, web: ``},
+	{name: `Ubytování u Křiváků`, type: `APARTMENTS`, price: `300`, stars: `1`, rate: ``, pictures: [], coords: `50°52'25.839\"N,14°14'25.764\"E`, web: ``},
+	{name: `Hotel u Lípy`, type: `HOTEL`, price: `1500`, stars: `2`, rate: `8.2`, pictures: `http://hotel-u-lipy.cz/wp-content/uploads/2015/05/cely_hotel_zboku1.jpg`, coords: `50°52'26.306\"N,14°14'30.124\"E`, web: `http://hotel-u-lipy.cz`},
+	{name: `Penzion Falk`, type: `HOTEL`, price: `300`, stars: `2`, rate: ``, pictures: `https://d48-a.sdn.szn.cz/d_48/c_img_H_D/hGRbJj.jpeg`, coords: `50°52'25.509\"N,14°14'32.242\"E`, web: `www.falk-hrensko.cz`},
+	{name: `Penzion Stará plynárna`, type: `HOTEL`, price: `2952`, stars: ``, rate: `8.8`, pictures: `https://staraplynarna.cz/wp-content/gallery/pokoj-1/GZ0A4832.jpg`, coords: `50°52'25.152\"N,14°14'38.961\"E`, web: `staraplynarna.cz`},
+	{name: `Hotel Praha`, type: `HOTEL`, price: `1403`, stars: `3`, rate: `8.1`, pictures: `https://d48-a.sdn.szn.cz/d_48/c_img_G_C/avQbTi.jpeg?fl=res,400,225,3`, coords: `50°52'31.758\"N,14°14'50.895\"E`, web: `www.hotel-hrensko.cz`},
+	{name: `Ubytování Hřensko`, type: `HOTEL`, price: `650`, stars: ``, rate: ``, pictures: `http://www.pensionsoutesky.cz/images/pokoje/10.jpg`, coords: `50°52'25.317\"N,14°15'4.868\"E`, web: `www.pensionsoutesky.cz`},
+	{name: `Pension Klepáč`, type: `HOTEL`, price: `870`, stars: ``, rate: ``, pictures: `https://www.pension-klepac.cz/images/articles/IMG_0647.JPG`, coords: `50°52'27.514\"N,14°15'7.484\"E`, web: `www.pension-klepac.cz`},
+	{name: `Penzion u Marešů`, type: `HOTEL`, price: `455`, stars: `2`, rate: ``, pictures: `http://www.umaresu.cz/images/gallery2/02.jpg`, coords: `50°52'8.398\"N,14°17'48.448\"E`, web: `www.umaresu.cz`},
+	{name: `Penzion Na Vyhlídce`, type: `HOTEL`, price: `1045`, stars: `2`, rate: `8.5`, pictures: `https://d48-a.sdn.szn.cz/d_48/c_img_H_B/I0iRmn.jpeg`, coords: `50°52'6.929\"N,14°17'46.814\"E`, web: `www.penzionnavyhlidce.eu`},
+	{name: `Hotel The Forest Garden`, type: `HOTEL`, price: `1961`, stars: `4`, rate: `8.6`, pictures: `https://q-xx.bstatic.com/xdata/images/hotel/max500/2041781.jpg`, coords: `50°52'9.922\"N,14°17'56.502\"E`, web: `www.theforestgardenhotel.cz`},
+	{name: `Apartmány Petra`, type: `APARTMENTS`, price: `1264`, stars: `2`, rate: `8.4`, pictures: `https://q-xx.bstatic.com/xdata/images/hotel/max500/140579579.jpg`, coords: `50°52'3.125\"N,14°17'55.998\"E`, web: `www.apartmanypetra.cz`},
+	{name: `Hotel Hubert`, type: `HOTEL`, price: `530`, stars: ``, rate: `7.6`, pictures: `https://d48-a.sdn.szn.cz/d_48/c_img_F_F/Va2BkG.jpeg`, coords: `50°52'2.548\"N,14°18'5.096\"E`, web: `www.hotelhubert.eu`},
+	{name: `Penzion Trixi's`, type: `APARTMENTS`, price: ``, stars: ``, rate: `9.8`, pictures: `https://q-xx.bstatic.com/xdata/images/hotel/max500/50444007.jpg`, coords: `50°51'59.829\"N,14°18'7.602\"E`, web: ``},
+	{name: `Penzion U Fořta`, type: `HOTEL`, price: `1991`, stars: `4`, rate: `8.9`, pictures: `https://q-xx.bstatic.com/xdata/images/hotel/max500/208283347.jpg`, coords: `50°52'25.798\"N,14°19'1.016\"E`, web: `www.uforta.cz`},
+	{name: `Hotel Mezní louka`, type: `HOTEL`, price: `1235 `, stars: `3`, rate: `8.1`, pictures: `https://q-xx.bstatic.com/xdata/images/hotel/max500/209116546.jpg`, coords: `50°52'24.383\"N,14°19'1.225\"E`, web: `www.meznilouka.cz`},
+	{name: `Kemp Mezní Louka`, type: `CAMPING`, price: `70`, stars: ``, rate: `7.7`, pictures: `https://d48-a.sdn.szn.cz/d_48/c_img_G_E/jxiMZc.mpo?fl=res,667,500,1`, coords: `50°52'26.182\"N,14°19'6.770\"E`, web: `www.uforta.cz`},
+	{name: `Penzion U Loupežáku`, type: `HOTEL`, price: `390`, stars: `2`, rate: `8.6`, pictures: `https://d48-a.sdn.szn.cz/d_48/c_img_G_C/3Je4DL.jpeg`, coords: `50°51'42.278\"N,14°21'18.695\"E`, web: `www.uloupezakusweb.webmium.com`},
+	{name: `Intercamp Mosquito`, type: `CAMPING`, price: `100`, stars: ``, rate: ``, pictures: `https://d48-a.sdn.szn.cz/d_48/c_img_G_C/UoB3Sc.mpo?fl=res,400,225,3`, coords: `50°51'16.927\"N,14°21'28.559\"E`, web: `www.mosquito.cz`},
+]
+
+var center = SMap.Coords.fromWGS84(14.3, 50.88);
+var map = new SMap(JAK.gel("map"), center, 14);
+map.addDefaultLayer(SMap.DEF_TURIST).enable();
+map.addDefaultControls();
+
+//var obrazek = "https://api.mapy.cz/img/api/marker/drop-red.png";
+const obrazek = `./img/pin.png`;
+
+var znacky = [];
+var souradnice = [];
+
+hotels.forEach((item) => {
+	const name = item.name;
+	const coords = item.coords;
+	var c = SMap.Coords.fromWGS84(coords);
+	console.log(coords);
+
+	var options = {
+		url:obrazek,
+		title:name,
+		anchor: {left:10, bottom: 1}  /* Ukotvení značky za bod uprostřed dole */
+}
+
+var znacka = new SMap.Marker(c, null, options);
+souradnice.push(c);
+znacky.push(znacka);
+
+})
+
+var decorationOptions = {
+    anchor: {left:0.5, top:0.5}
+}
+znacky[1].decorate(SMap.Marker.Feature.RelativeAnchor, decorationOptions);
+
+var vrstva = new SMap.Layer.Marker();     /* Vrstva se značkami */
+map.addLayer(vrstva);                          /* Přidat ji do mapy */
+vrstva.enable();                         /* A povolit */
+for (var i=0;i<znacky.length;i++) {
+    vrstva.addMarker(znacky[i]);
+}
+
+var cz = map.computeCenterZoom(souradnice); /* Spočítat pozici mapy tak, aby značky byly vidět */
+map.setCenterZoom(cz[0], cz[1]);        
